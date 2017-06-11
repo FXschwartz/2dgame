93d2f2dc6ff8c57cd628333b82e99cd54d2daaec
Sergei <sergei@sleekbook>
2017-06-11 18:35:21 -0500

    WIP on using the phaser-tiled plugin for additional speed.

--- js/game.js
+++ js/game.js
@@ -13,6 +13,7 @@ class Game extends Phaser.Game {
 class PlayState extends Phaser.State {
 	preload() {
 		this.game.add.plugin(Phaser.Plugin.Debug);
+		this.game.add.plugin(Phaser.Plugin.Tiled);
 
 		// this.game.load.image('sky', 'assets/sky.png');
 		// this.game.load.image('ground', 'assets/platform.png');
@@ -23,14 +24,19 @@ class PlayState extends Phaser.State {
 		this.game.load.spritesheet('health', 'assets/health.png', 64,64);
 		// this.game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
 
-		this.game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-		this.game.load.image('tiles_spritesheet', 'assets/tiles_spritesheet.png');
+		// this.game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+		// this.game.load.image('tiles_spritesheet', 'assets/tiles_spritesheet.png');
+
+		var cacheKey = Phaser.Plugin.Tiled.utils.cacheKey;
+		this.game.load.tiledmap(cacheKey('level1', 'tiledmap'), 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.image(cacheKey('level1', 'tileset', 'tiles_spritesheet'), 'assets/tiles_spritesheet.png');
 	}
 
 	create() {
 		// console.log('create()\t this=%o', this);
 		// this.game.world.setBounds(0, 0, 2000, 2000);
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		// this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.game.physics.startSystem(Phaser.Physics.P2JS);
 
 		this.game.level = new Level(this.game, 'level1');
 	}
--- js/level.js
+++ js/level.js
@@ -2,15 +2,19 @@ class Level {
 	constructor(game, tilemap) {
 		this.game = game;
 
-		this.map = this.game.add.tilemap(tilemap);
-		this.map.addTilesetImage('tiles_spritesheet', 'tiles_spritesheet');
-
 		this.game.stage.backgroundColor = "#a9f0ff";
 
-		this.backgroundlayer = this.map.createLayer('BackgroundLayer');
-		this.groundLayer = this.map.createLayer('GroundLayer');
-		this.groundLayer.resizeWorld();
-		this.map.setCollisionBetween(0,200, true, 'GroundLayer');
+		// this.map = this.game.add.tilemap(tilemap);
+		// this.map.addTilesetImage('tiles_spritesheet', 'tiles_spritesheet');
+
+		this.map = game.add.tiledmap(tilemap);
+
+		// this.backgroundlayer = this.map.createLayer('BackgroundLayer');
+		// this.groundLayer = this.map.createLayer('GroundLayer');
+		// this.groundLayer.resizeWorld();
+		// this.map.setCollisionBetween(0,200, true, 'GroundLayer');
+
+		game.physics.p2.convertTiledmap(this.map, 'GroundLayer');
 
 		this.player = new Player(this.game);
 	}
